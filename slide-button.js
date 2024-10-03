5d691a7a8f556641743641e806dc254e28bd5cdd
Siael Alves <siaelalves@outlook.com>
2024-10-03 16:23:18 -0300

    FEAT Adicionado efeito "readonly"; - Agora é possível definir um breve intervalo entre o clique e a ativação do botão;

--- slide-button.js
+++ slide-button.js
@@ -1,22 +1,45 @@
 function toggleActivate() {
+ let loadingTime = 500;
  let button = document.querySelector("button.slideButton");
  let slider = document.querySelector("button.slideButton div.slider");
 
  if(button.classList.contains("off")){
-  button.classList.remove("off");
-  button.classList.add("on");
+  button.style.cursor = "wait";
+  button.classList.add("readonly")
+  slider.classList.add("readonly");
 
-  slider.classList.remove("off");
-  slider.classList.add("on");
+  setTimeout(()=>{
+   button.classList.remove("readonly");
+   slider.classList.remove("readonly");
+   button.style.cursor = "pointer";
+
+   button.classList.remove("off");
+   button.classList.add("on");
+ 
+   slider.classList.remove("off");
+   slider.classList.add("on");
+  },loadingTime);
+
   return;
  } 
 
  if(button.classList.contains("on")){
-  button.classList.remove("on");
-  button.classList.add("off");
+  button.style.cursor = "wait";
+  button.classList.add("readonly");
+  slider.classList.add("readonly");
 
-  slider.classList.remove("on");
-  slider.classList.add("off");
+  setTimeout(()=>{
+   button.classList.remove("readonly");
+   slider.classList.remove("readonly");
+   button.style.cursor = "pointer";
+
+   button.classList.remove("on");
+   button.classList.add("off");
+ 
+   slider.classList.remove("on");
+   slider.classList.add("off");
+  },loadingTime);
+
   return;
  }
 }
